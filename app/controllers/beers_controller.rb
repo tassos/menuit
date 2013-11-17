caf398d6de37eaef6387234a707c832f4290ea00
Tassos (nakano) Natsakis <natsakis.anastasios@gmail.com>
2013-11-17 22:57:39 +0100

    Possibility to add new beers in the database, and to index all the existing ones

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,4 +1,16 @@
 class BeersController < ApplicationController
+  def new
+    @beer = Beer.new(params[:id])
+  end
+  def show
+    @beer = Beer.find(params[:id])
+  end
   def create
+    @beer = Beer.new(params[:beer].permit(:name, :brewery, :vol))
+    @beer.save
+    redirect_to @beer
+  end
+  def index
+    @beers = Beer.all
   end
 end
